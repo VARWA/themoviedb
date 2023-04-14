117f92c265fbc3b10b3cd7bbaf05fd0d99f49876
VARWA <60575285+VARWA@users.noreply.github.com>
2023-04-14 22:00:29 +0300

    Update movie_list_widget.dart

--- lib/widgets/movie_list/movie_list_widget.dart
+++ lib/widgets/movie_list/movie_list_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:themoviedb/Library/Widgets/Inhereted/provider.dart';
 import 'package:themoviedb/domain/api_client/api_client.dart';
 import 'movie_list_model.dart';
