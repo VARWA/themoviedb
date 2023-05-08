d2c5aaa088fd6ab2a530f0b5f171a67a470a15c3
Богдан Лупанов <60575285+VARWA@users.noreply.github.com>
2023-05-09 00:16:16 +0300

    lesson 74

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:themoviedb/Library/Widgets/Inhereted/provider.dart';
 import 'package:themoviedb/Theme/app_colors.dart';
 import 'package:themoviedb/ui/navigation/main_navigation.dart';
 import 'package:themoviedb/widgets/app/my_app_model.dart';
@@ -8,18 +9,21 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final model = MyAppModel();
   await model.checkauth();
-  runApp(MyApp(model: model));
+  const app = MyApp();
+  final widget = Provider(model: model, child: app);
+  runApp(widget);
 }
 
 class MyApp extends StatelessWidget {
-  final MyAppModel model;
+  // final MyAppModel model;
 
   static final mainNavigation = MainNavigation();
 
-  const MyApp({Key? key, required this.model}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final model = Provider.read<MyAppModel>(context);
     return MaterialApp(
       title: "Flutter Demo",
       theme: ThemeData(
@@ -31,17 +35,17 @@ class MyApp extends StatelessWidget {
             selectedItemColor: Colors.white,
             unselectedItemColor: Colors.grey),
       ),
-      localizationsDelegates: [
+      localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: [
+      supportedLocales: const [
         Locale('ru'), // English
         Locale('en'), // Spanish
       ],
       routes: mainNavigation.routes,
-      initialRoute: mainNavigation.initialRoute(model.isAuth),
+      initialRoute: mainNavigation.initialRoute(model?.isAuth==true),
       onGenerateRoute: mainNavigation.onGenerateRoute,
     );
   }
