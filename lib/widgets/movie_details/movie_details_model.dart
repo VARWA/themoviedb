f787c0e0a033b7120c40825f851bb6fbcf515902
Богдан Лупанов <60575285+VARWA@users.noreply.github.com>
2023-05-08 22:51:34 +0300

    lesson73

--- lib/widgets/movie_details/movie_details_model.dart
+++ lib/widgets/movie_details/movie_details_model.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:themoviedb/domain/api_client/api_client.dart';
+import 'package:themoviedb/domain/data_providers/session_data_provider.dart';
 import 'package:themoviedb/domain/entity/movie_details.dart';
 
 class MovieDetailsModel extends ChangeNotifier {
+  final _sessionDataProvider = SessionDataProvider();
   final _apiClient = ApiClient();
 
   final int movieId;
   MovieDetails? _movieDetails;
+  bool _isFavorite = false;
   String _locale = '';
   late final DateFormat _dateFormat;
 
   MovieDetails? get movieDetails => _movieDetails;
+  bool get isFavorite => _isFavorite;
 
   MovieDetailsModel(this.movieId);
 
@@ -28,6 +32,25 @@ class MovieDetailsModel extends ChangeNotifier {
 
   Future<void> loadDetails() async {
     _movieDetails = await _apiClient.movieDetails(movieId, _locale);
+    final sessionId = await _sessionDataProvider.getSessionId();
+    if (sessionId != null) {
+      _isFavorite = await _apiClient.isFavorite(movieId, sessionId);
+    }
     notifyListeners();
+  }
+
+  Future<void> toggleFavorite() async {
+    final accountId = await _sessionDataProvider.getAccountId();
+    final sessionId = await _sessionDataProvider.getSessionId();
+    if (accountId == null || sessionId == null) return;
+    _isFavorite = !_isFavorite;
+    notifyListeners();
+    await _apiClient.markAsFavorite(
+      accountId: accountId,
+      sessionId: sessionId,
+      mediaType: MediaType.Movie,
+      mediaId: movieId,
+      isFavorite: _isFavorite,
+    );
   }
 }
